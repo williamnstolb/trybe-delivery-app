46fc67b343171720c1134011841830fcba58df9f
Samuel Nascimento <sfsnascimento@gmail.com>
2022-04-29 17:05:25 -0300

    creating controllers user and product

---
+++ back-end/src/controller/userController.js
@@ -0,0 +1,24 @@
+const user = require('../service/userService');
+
+const login = async (req, res) => {
+  const userInfo = req.body;
+  const userLogged = await user.login(userInfo);
+
+  if (userLogged.code) return res.status(userLogged.code).json({ messasge: userLogged.message });
+
+  res.status(200).json(userLogged);
+};
+
+const register = async (req, res) => {
+  const userInfo = req.body;
+  const result = await user.register(userInfo);
+
+  if (result.code) return res.status(result.code).json({ message: result.message });
+
+  res.status(200).json(result);
+};
+
+module.exports = {
+  login,
+  register,
+};
