dc0d5112d58a1ccccb8dc78cea4f82b33edc6f58
Heitor SF <heitor.sf5@gmail.com>
2022-05-03 23:46:50 -0300

    fixed fields in Products, Sales and ProductSales

--- back-end/src/database/models/Products.js
+++ back-end/src/database/models/Products.js
@@ -2,8 +2,10 @@ module.exports = (sequelize, DataTypes) => {
   const product = sequelize.define('Product', {
     name: DataTypes.STRING(100),
     price: DataTypes.DECIMAL(4, 2),
-    url_image: DataTypes.STRING(200)
+    urlImage: {
+      type: DataTypes.STRING(200),
+      field: 'url_image'
+    }
   }, { timestamps: false });
-  // afsdasgdsg
   return product;
 };
