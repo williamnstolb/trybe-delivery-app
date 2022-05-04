7d1dfc372a0f9561552d206e468eafb286a56c2b
Heitor SF <heitor.sf5@gmail.com>
2022-05-04 18:01:53 -0300

    added correct data-testids for prodCard

--- front-end/src/components/ProductCard.jsx
+++ front-end/src/components/ProductCard.jsx
@@ -5,27 +5,40 @@ import PropTypes from 'prop-types';
 function ProductCard({ prodData }) {
   const { id, name, price, urlImage } = prodData;
   console.log('\n\n\n EVERY INFO: ', prodData);
+
+
+
   return (
     <div
       className="cardFrame"
-      // data-testid={ `customer_products__element-card-title-${id}` }
+      // data-testid={ `customer_products__element-card-title-${name}` }
     >
       <img
         src={ urlImage }
         alt={ name }
-        data-testid={ `customer_products__element-card-img-${urlImage}` }
+        data-testid={ `customer_products__img-card-bg-image-${id}` }
       />
-      <p data-testid={ `customer_products__element-card-title-${name}` }>
+      <p data-testid={ `customer_products__element-card-title-${id}` }>
         { name }
       </p>
       <p data-testid={ `customer_products__element-card-price-${id}` }>
         { price }
       </p>
+
       <button
         type="button"
         data-testid={ `customer_products__button-card-add-item-${id}` }
       >
         ADICIONAR
+      </button>
+      <p data-testid={ `customer_products__input-card-quantity-${id}` }>
+        0
+      </p>
+      <button
+        type="button"
+        data-testid={ `customer_products__button-card-rm-item-${id}` }
+      >
+        REMOVER
       </button>
     </div>
   );
