10f78359daa4ec2e26d488ae1a0f56f08164b6e8
AnaPaulaBraga <anapaula.junq@hotmail.com>
2022-05-05 17:54:41 -0300

    cria componente checkoutList

---
+++ front-end/src/components/CheckoutList.jsx
@@ -0,0 +1,81 @@
+import React, { useEffect } from 'react';
+
+function CheckoutList() {
+//   const [produtos, setProdutos] = useState([]);
+//   const [totalPrice, setTotalPrice] = useState(0);
+
+  useEffect(() => {
+    // pegar no localstorage 'carrinho' ??
+    // setTotalPrice(price);?
+    // setProdutos(pedidos);?
+  }, []);
+
+  const handleRemove = () => {
+  };
+
+  return (
+    <div>
+      <h2>Finalizar Pedido</h2>
+      <table>
+        <thead>
+          <tr>
+            <td>Item</td>
+            <td>Descrição</td>
+            <td>Quantidade</td>
+            <td>Valor Unitário</td>
+            <td>Sub-Total</td>
+            <td>Remover Item</td>
+          </tr>
+        </thead>
+        <tbody>
+          {produtos.map(({ id, price, name, quantity }, it) => (
+            <tr key={ it }>
+              <td
+                data-testid={ `customer_checkout__element-order-table-item-number-${it}` }
+              >
+                {it + 1}
+              </td>
+              <td
+                data-testid={ `customer_checkout__element-order-table-name-${it}` }
+              >
+                {name}
+              </td>
+              <td
+                data-testid={ `customer_checkout__element-order-table-quantity-${it}` }
+              >
+                {quantity}
+              </td>
+              <td
+                data-testid={ `customer_checkout__element-order-table-unit-price-${it}` }
+              >
+                {price}
+              </td>
+              <td
+                data-testid={ `customer_checkout__element-order-table-sub-total-${it}` }
+              >
+                {(price * quantity).toFixed(2)}
+              </td>
+              <td>
+                <button
+                  onClick={ handleRemove }
+                  id={ id }
+                  data-testid={ `customer_checkout__element-order-table-remove-${it}` }
+                  type="button"
+                >
+                  Remover Item
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <span
+        data-testid="customer_checkout__element-order-total-price"
+      >
+        { `Total: R$ ${totalPrice}` }
+      </span>
+    </div>
+  );
+}
+
+export default CheckoutList;
