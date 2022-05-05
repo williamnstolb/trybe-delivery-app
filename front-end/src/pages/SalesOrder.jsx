d17d319852b8a8e5206fd6e89cfe719b69d00985
Williamns <williamnstolb@gmail.com>
2022-05-05 15:53:13 -0300

    criação da função getSales

--- front-end/src/pages/SalesOrder.jsx
+++ front-end/src/pages/SalesOrder.jsx
@@ -3,22 +3,31 @@ import Navbar from '../components/NavBar';
 import SalesCard from '../components/SalesCard';
 
 function SalesOrder() {
-  const [sales] = useState([
-    {
-      status: 'Pendente',
-      deliveryNumber: '12345',
-      deliveryAddress: 'Rua dos bobos, 0',
-      totalPrice: 'R$ 10,00',
-      salesDate: '01/01/2020',
-    },
-    {
-      status: 'Entregue',
-      deliveryNumber: '12',
-      deliveryAddress: 'Rua dos bobos, 1',
-      totalPrice: 'R$ 90,00',
-      salesDate: '04/05/2022',
-    },
-  ]);
+  const [sales, setSales] = useState();
+
+  const getSales = async () => {
+    // const response = await fetch('/api/sales');
+    // const data = await response.json();
+    const data = [
+      {
+        status: 'Pendente',
+        deliveryNumber: '12345',
+        deliveryAddress: 'Rua dos bobos, 0',
+        totalPrice: 'R$ 10,00',
+        salesDate: '01/01/2020',
+      },
+      {
+        status: 'Entregue',
+        deliveryNumber: '12',
+        deliveryAddress: 'Rua dos bobos, 1',
+        totalPrice: 'R$ 90,00',
+        salesDate: '04/05/2022',
+      },
+    ];
+
+    setSales(data);
+  };
+
   return (
     <div>
       <Navbar />
