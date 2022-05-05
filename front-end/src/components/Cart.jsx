d1418b11ce3486b4e0c125f5324199bde1258aff
Heitor SF <heitor.sf5@gmail.com>
2022-05-05 19:54:48 -0300

    STYLE FIXED SHUT UP LINT

--- front-end/src/components/Cart.jsx
+++ front-end/src/components/Cart.jsx
@@ -4,9 +4,11 @@ export const getCart = () => {
   console.log('IN getCart');
   const myCart = JSON.parse(localStorage.getItem('cartData'));
   if (!myCart) {
+    console.log('LOCAL CART WAS EMPTY!');
     localStorage.setItem('cartData', JSON.stringify([]));
     return [];
   }
+  console.log('local cart was NOT empty!');
   // const newWay = localStorage.userData();
   console.log('\n\n\n current Cart:', myCart);
   // console.log('\n userData:', localStorage.getItem('userData'));
@@ -19,7 +21,6 @@ export const setCart = (item) => {
   // const newCart = [...myCart, item];
   const newCart = item;
   JSON.stringify(localStorage.setItem('cartData', newCart));
-  // alert('ADD ITEM LOGIC HERE');
 };
 
 export const removeItem = ({ id }) => {
@@ -27,7 +28,6 @@ export const removeItem = ({ id }) => {
   const itemRemoved = myCart.filter((item) => item.id !== id);
   JSON.stringify(localStorage.setItem('cartData', itemRemoved));
   console.log('\n\n Removing the item: ', id, '\n newCart: ', itemRemoved);
-  // alert('REMOVE ITEM LOGIC HERE');
 };
 
 // cartData from storage is an array
