e194a594e89e7bb18cb89cbe6cbb6c2575af7196
Williamns <williamnstolb@gmail.com>
2022-05-06 19:55:54 -0300

    data-testid sendo colocado

--- front-end/src/components/DetailsCard.jsx
+++ front-end/src/components/DetailsCard.jsx
@@ -1,60 +1,87 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button } from 'react-bootstrap';
 
-function DetailsCard({ dataOrder }) {
+function DetailsCard({ data }) {
   const {
+    role,
     status,
     deliveryNumber,
     salesDate,
     order,
-  } = dataOrder;
+  } = data;
+  const total = order.reduce((acc, curr) => acc + curr.price * curr.quantity, 0);
 
   return (
     <div>
       <h5>Detalhe do Pedido</h5>
-      {/* <div className="card-body">
-        <div className="row">
-          <div className="col-md-6">
+
+      <div className="card card-body bg-primary mb-3">
+        <div className="row row-cols-2 row-cols-lg-5 g-2 g-lg-3">
+          <p
+            className="card text-primary text-center"
+            data-testid={
+              `${role}_order_details__element-order-details-label-order-${id}` }
+          >
             Pedido
             {' '}
-            { deliveryNumber }
-          </div>
-          <div className="col-md-6">{ salesDate }</div>
-          <div className="col-md-6">{ status }</div>
-          <Button variant="primary">PREPARAR PEDIDO</Button>
-          <Button variant="secundary">SAIU PARA ENTREGA</Button>
+            {deliveryNumber}
+          </p>
+          <p className="card text-primary text-center">{ salesDate }</p>
+          <p className="card text-primary text-center">{ status }</p>
+          <button type="button" className="btn btn-warning btn-sm">
+            <h6 className="fs-6">PREPARAR PEDIDO</h6>
+          </button>
+          <button type="button" className="btn btn-success btn-sm">
+            <h6 className="fs-6">PREPARAR PEDIDO</h6>
+          </button>
         </div>
-        <div className="row">
-          <p className="col-md-12">Item</p>
-          <p className="col-md-12">Descrição</p>
-          <p className="col-md-12">Quantidade</p>
-          <p className="col-md-12">Valor Unitário</p>
-          <p className="col-md-12">Sub Total</p>
+        <div className="row row-cols-2 row-cols-lg-5 g-2 g-lg-3">
+          <p>Item</p>
+          <p>Descrição</p>
+          <p>Quantidade</p>
+          <p>Valor Unitário</p>
+          <p>Sub-total</p>
         </div>
-        { order.map((item) => (
-          <div className="row" key={ item.id }>
-            <p className="col-md-12">{ item.id }</p>
-            <p className="col-md-12">{ item.name }</p>
-            <p className="col-md-12">{ item.quantity }</p>
-            <p className="col-md-12">{ item.price }</p>
-            <p className="col-md-12">{ item.quantity * item.price }</p>
+        {order.map((item) => (
+          <div className="row row-cols-2 row-cols-lg-5 g-2 g-lg-3" key={ item.id }>
+            <p className="card text-primary text-center">{item.id}</p>
+            <p className="card text-primary text-center">{item.name}</p>
+            <p className="card text-primary text-center">{item.quantity}</p>
+            <p className="card text-primary text-center">
+              R$
+              { ' ' }
+              {item.price}
+
+            </p>
+            <p className="card text-primary text-center">
+              R$
+              { ' ' }
+              {(item.quantity * item.price).toFixed(2)}
+            </p>
           </div>
-        )) }
-      </div> */}
+        ))}
+        <div className="">
+          Total:
+          { ' ' }
+          R$
+          { ' ' }
+          {total.toFixed(2)}
+        </div>
+      </div>
     </div>
   );
 }
 
 DetailsCard.propTypes = {
-  dataOrder: PropTypes.shape({
+  data: PropTypes.shape({
+    role: PropTypes.string.isRequired,
     status: PropTypes.string.isRequired,
     deliveryNumber: PropTypes.string.isRequired,
     salesDate: PropTypes.string.isRequired,
     order: PropTypes.arrayOf(PropTypes.shape({
       id: PropTypes.number.isRequired,
       name: PropTypes.string.isRequired,
-      price: PropTypes.string.isRequired,
+      price: PropTypes.number.isRequired,
       quantity: PropTypes.number.isRequired,
     })).isRequired,
   }).isRequired,
