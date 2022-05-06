735cb7ab96e9ec9ccce76973d11662f6c26e7b4a
Williamns <williamnstolb@gmail.com>
2022-05-06 19:55:24 -0300

    detailsCard criado

--- front-end/src/pages/SalesDetails.jsx
+++ front-end/src/pages/SalesDetails.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DetailsCard from '../components/DetailsCard';
 import Navbar from '../components/NavBar';
 
 function SalesDetails() {
-  const dataOrder = {
+  const [dataOrder] = useState({
+    role: 'seller',
     status: 'Pendente',
     deliveryNumber: '12345',
     salesDate: '01/01/2020',
@@ -11,17 +12,46 @@ function SalesDetails() {
       {
         id: 1,
         name: 'Produto 1',
-        price: 'R$ 10,00',
+        price: 10,
         quantity: 1,
       },
       {
         id: 2,
         name: 'Produto 2',
-        price: 'R$ 10,00',
-        quantity: 1,
+        price: 10,
+        quantity: 3,
       },
     ],
-  };
+
+  });
+
+  // async function getDetailsOrder() {
+  //   const data = {
+  //     status: 'Pendente',
+  //     deliveryNumber: '12345',
+  //     salesDate: '01/01/2020',
+  //     order: [
+  //       {
+  //         id: 1,
+  //         name: 'Produto 1',
+  //         price: 'R$ 10,00',
+  //         quantity: 1,
+  //       },
+  //       {
+  //         id: 2,
+  //         name: 'Produto 2',
+  //         price: 'R$ 10,00',
+  //         quantity: 1,
+  //       },
+  //     ],
+  //   };
+
+  //   setDataOrder(data);
+  // }
+
+  // useEffect(() => {
+  //   getDetailsOrder();
+  // }, []);
 
   return (
     <div>
