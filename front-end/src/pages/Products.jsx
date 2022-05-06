1c71519e9ce4d365c3befb4e159a6360c3cf689b
Heitor SF <heitor.sf5@gmail.com>
2022-05-06 04:39:16 -0300

    req 16 done, hopefully. Finishing 15

--- front-end/src/pages/Products.jsx
+++ front-end/src/pages/Products.jsx
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import Navbar from '../components/NavBar';
 import api from '../services/api';
 import ProductCard from '../components/ProductCard';
 import '../styles/pages/products.css';
+import { getCart } from '../components/Cart';
 
 function Products() {
   // const [load, setLoad] = useState(true);
   const [myProds, setProds] = useState([]);
+  const [finalPrice, setPrice] = useState('0,00');
 
   async function fetchProducts() {
     const response = await api.get('products');
@@ -17,8 +20,36 @@ function Products() {
     return allProducts;
   }
 
+  function calculatePrice() {
+    const myCart = getCart();
+    if (myCart.length < 1) return 0;
+    const calcPrice = myCart
+      .map(({ price, itemQty }) => parseInt(itemQty, 10) * parseFloat(price))
+      .reduce((prevVal, currVal) => prevVal + currVal);
+    return setPrice(calcPrice.toFixed(2));
+  }
+
+  function checkoutField() {
+    return (
+      <footer>
+        <button
+          type="button"
+          data-testid="customer_products__button-cart"
+        >
+          <Link to="/checkout">CHECKOUT</Link>
+        </button>
+        <p data-testid="customer_products__checkout-bottom-value">
+          R$
+          { finalPrice }
+        </p>
+      </footer>
+    );
+    // Ele consta como footer mas eu coloquei no topo da pagina pra ficar mais facil de acessar ele durante o desenvolvimento
+  }
+
   useEffect(() => {
     fetchProducts();
+    calculatePrice();
   }, []);
 
   // if (load) return <p> LOADING </p>;
@@ -26,6 +57,7 @@ function Products() {
     <div>
       PRODUCTS
       <Navbar pageName="Produtos" />
+      { checkoutField() }
       <span className='="productList'>
         { myProds }
       </span>
