66abc09c2c41bdf782565e6dc2113825ac74cdd2
AnaPaulaBraga <anapaula.junq@hotmail.com>
2022-05-09 17:09:14 -0300

    atualiza app.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,12 +3,9 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Products from './pages/Products';
 import Register from './pages/Register';
-<<<<<<< HEAD
-import Checkout from './pages/Checkout';
-=======
 import SalesOrder from './pages/SalesOrder';
 import SalesDetails from './pages/SalesDetails';
->>>>>>> 08d107c23fe02d041288639655d4f75439393cb8
+import Checkout from './pages/Checkout';
 
 // teste
 function App() {
@@ -20,12 +17,9 @@ function App() {
         <Route path="/register" element={ <Register /> } />
         <Route path="/products/" element={ <Navigate to="/customer/products" /> } />
         <Route exact path="/customer/products" element={ <Products /> } />
-<<<<<<< HEAD
         <Route exact path="/customer/checkout" element={ <Checkout /> } />
-=======
         <Route path="/seller/orders" element={ <SalesOrder /> } />
         <Route path="/seller/orders/:id" element={ <SalesDetails /> } />
->>>>>>> 08d107c23fe02d041288639655d4f75439393cb8
       </Routes>
     </BrowserRouter>
   );
