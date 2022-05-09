93c3d42253c3568fe65e79bf572c5cab7fb9ee16
Samuel Nascimento <sfsnascimento@gmail.com>
2022-05-09 15:51:14 -0300

    adding create function in saleService

--- back-end/src/service/saleService.js
+++ back-end/src/service/saleService.js
@@ -1,4 +1,4 @@
-const { Sale } = require('../database/models');
+const { Sale, SalesProduct } = require('../database/models');
 
 const getById = async (id) => {
   const response = await Sale.findAll({
@@ -8,6 +8,21 @@ const getById = async (id) => {
   return response;
 };
 
+const create = async (saleData) => {
+  const { sale, saleProduct } = saleData;
+
+  const { id } = await Sale.create(sale);
+  
+  await Promise.all(
+    saleProduct.map(async (sale) => {
+      await SalesProduct.create(sale);
+    })
+  );
+
+  return id;
+}
+
 module.exports = {
   getById,
+  create,
 };
