aba7dcac7d0cc79e56aeb91d2eb76a618a015fa4
Samuel Nascimento <sfsnascimento@gmail.com>
2022-05-09 15:50:42 -0300

    adding create function in saleController

--- back-end/src/controller/saleController.js
+++ back-end/src/controller/saleController.js
@@ -8,6 +8,15 @@ const getById = async (req, res) => {
   res.status(200).json(response);
 };
 
+const create = async (req, res) => {
+  const saleData = req.body;
+
+  const id = await sale.create(saleData);
+
+  res.status(201).json(id);
+}
+
 module.exports = {
   getById,
+  create,
 };
