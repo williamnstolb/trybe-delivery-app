8a94b2a4646b961207cca377249831669b9e9c43
Williamns <williamnstolb@gmail.com>
2022-05-09 18:31:29 -0300

    Criação de um componente separado pra endereço

---
+++ front-end/src/components/orders/Address.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Address({ data }) {
+  const { id, deliveryAddress } = data;
+  return (
+    <div>
+      <p
+        className="card text-primary text-center"
+        data-testid={ `${role}_orders__element-card-address-${id}` }
+      >
+        {deliveryAddress}
+      </p>
+    </div>
+  );
+}
+
+Address.propTypes = {
+  data: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    deliveryAddress: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default Address;
