e3f839b54476089d48bf9f22809bedc4acc7554a
Williamns <williamnstolb@gmail.com>
2022-05-13 15:56:08 -0300

    fix: backend para retornar dados corretos

--- back-end/src/controller/customerController.js
+++ back-end/src/controller/customerController.js
@@ -2,7 +2,6 @@ const sale = require('../service/customerService');
 
 const getByCustomerId = async (req, res) => {
   const { id } = req.params;
-  
   const response = await sale.getByCustomerId(id);
 
   res.status(200).json(response);
--- back-end/src/service/customerService.js
+++ back-end/src/service/customerService.js
@@ -1,10 +1,15 @@
-const { Sale } = require('../database/models');
+const { Sale, Product } = require('../database/models');
 
 const getByCustomerId = async (id) => {
   const response = await Sale.findAll({
     where: { userId: id },
+    include: [{
+      model: Product,
+      as: 'orders',
+      attributes: ['id', 'name', 'price'],
+      through: { attributes: ['quantity'], as: 'salesProduct' },
+    }],
   });
-
   return response;
 };
 
