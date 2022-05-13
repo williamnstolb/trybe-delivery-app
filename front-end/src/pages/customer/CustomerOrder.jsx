11e48eaabffab26376d6be81f21468d1a7fc7236
Williamns <williamnstolb@gmail.com>
2022-05-13 16:44:47 -0300

    retirando dataMocked da importação

--- front-end/src/pages/customer/CustomerOrder.jsx
+++ front-end/src/pages/customer/CustomerOrder.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import NavBar from '../../components/NavBar';
 import Loading from '../../components/Loading';
 import OrderCard from '../../components/OrderCard';
-import dataMocked from '../../data/dataMocked';
 import api from '../../services/api';
 
 function CustomerOrder() {
